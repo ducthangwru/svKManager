00d4b5dfbbcac89bfa947fd14400f382a9c6318c
DucThangWRU <ducthangwru97@gmail.com>
2018-02-05 08:39:19 +0700

    update

---
+++ modules/api/users/usersController.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const Router = express.Router();
+const usersModel = require('./usersModel');
+const menusModel = require('../menus/menusModel');
+const config = require('../../../configString.json');
+const Utils = require('../../../utils/Utils');
+
+Router.post('/', (req, res) => {
+    try
+    {
+        let newUser = {
+            // idlogin : req.body.idlogin,
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            avatar : req.body.avatar,
+            tokenfirebase : req.body.tokenfirebase,
+            fullname: req.body.fullname,
+            dateofbirth: req.body.dateofbirth,
+            status : true,
+            group :"5a6fe111734d1d63031a767a"
+        };
+    
+        // if(!Utils.verifyLogin(req.body.idlogin, req.headers['token']))
+        // {
+        //     res.send({status : false, msg : config.MA_TOKEN_KHONG_DUNG});
+        // }
+        // else
+        // {
+            usersModel.createUser(newUser, (err, doc) => {
+                    if (err != null) {
+                        console.log(err);
+                        res.send({ status : false, msg : config.KHONG_THANH_CONG});
+                    } else {
+                        res.send({ status : true, msg : config.THANH_CONG});
+                    }
+                }
+            )
+        //}
+    }
+    catch(err)
+    {
+        res.send({status : false, msg : config.CO_LOI_XAY_RA});
+    }
+});
+
+Router.put('/', (req, res) => {
+    try
+    {
+        let newUser = {
+            idlogin : req.body.idlogin,
+            id: req.body._id,
+            password: req.body.password,
+            email: req.body.email,
+            avatar : req.body.avatar,
+            tokenfirebase : req.body.tokenfirebase,
+            fullname: req.body.fullname,
+            dateofbirth: req.body.dateofbirth,
+            status : req.body.status,
+            group : req.body.group
+        };
+        if(!Utils.verifyLogin(req.body.idlogin, req.headers['token']))
+        {
+            res.send({status : false, msg : config.MA_TOKEN_KHONG_DUNG});
+        }
+        else
+        {
+            usersModel.updateUser(newUser, (err, doc) => {
+                if (err != null) {
+                    res.send({ status : false, msg : config.KHONG_THANH_CONG});
+                } else {
+                    res.send({ status : true, msg : config.THANH_CONG});
+                }
+            });
+        }
+    }
+    catch(err)
+    {
+        res.send({status : false, msg : config.CO_LOI_XAY_RA});
+    }
+});
+
+Router.post('/login', (req, res) => {
+    try
+    {
+        let user = {
+            username: req.body.username,
+            password: req.body.password
+        }
+
+        usersModel.selectUser(user, async (err, doc) => {
+            if (err != null) {
+                res.send({ status : false, msg : config.KHONG_THANH_CONG, data : null, token : ""});
+            } else {
+                var token = Utils.getToken(doc._id);
+                let menus = await menusModel.findAllMenus({});
+                res.send({ status : true, msg : config.THANH_CONG, data : doc, token : token, menus : menus});
+            }
+        }
+    )
+    }
+    catch(err)
+    {
+        res.send({status : false, msg : config.CO_LOI_XAY_RA});
+    }
+});
+
+module.exports = Router;
---
+++ modules/api/users/usersModel.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+const userSchema = require('./usersSchema');
+let usersModel = mongoose.model('users', userSchema);
+
+const groupsSchema = require('../groups/groupsSchema');
+let groupsModel = mongoose.model('groups', groupsSchema);
+
+const createUser = (user, callback) => {
+    try
+    {
+        usersModel.create(user, (err, doc) => {
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, doc);
+            }
+        });
+    }
+    catch(err)
+    {
+        callback(err);
+    }
+}
+
+const updateUser = (user, callback) => {
+    try
+    {
+        var id = user._id;
+        var queryUpdate = {
+            email : user.email,
+            fullname : user.fullname,
+            dateofbirth : user.dateofbirth,
+            tokenfirebase : user.tokenfirebase,
+            avatar : user.avatar,
+            password : user.password,
+            email : user.email,
+            group : user.group
+        }
+    
+        usersModel.findOneAndUpdate(id, queryUpdate, (err, doc) => {
+            if (err) {
+                console.log(err);
+                callback(err);
+            } else {
+                callback(null, doc);
+            }
+        })
+    }
+    catch(err)
+    {
+        callback(err);
+    }
+}
+
+const selectUser = (user, callback) => {
+    try
+    {
+        var queryFind = {
+            username : user.username,
+            password : user.password,
+        }
+    
+        usersModel.findOne(queryFind, (err, doc) => {
+            if (err) {
+                console.log(err);
+                callback(err);
+            } else {
+                callback(null, doc);
+            }
+        }).populate({
+            path: 'group',
+            model: groupsModel 
+          }).exec();
+    }
+    catch(err)
+    {
+        callback(err);
+    }
+}
+
+module.exports = {
+    createUser, updateUser, selectUser
+}
