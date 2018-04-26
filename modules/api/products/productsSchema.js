845026a4e18500176711cc15576e1ca62e4f386e
ducthangwru <ducthangwru97@gmail.com>
2018-04-26 22:15:32 +0700

    ok babby

--- modules/api/products/productsSchema.js
+++ modules/api/products/productsSchema.js
@@ -6,7 +6,8 @@ const ObjectId = Schema.Types.ObjectId;
 const productsSchema = new Schema(
     {
         name : {type : String, require : true},
-        model : {type : String, require : true}
+        model : {type : String, require : true},
+        info : {type: String, default : ""}
     }, {timestamps : {createAt : 'created_at', updateAt : 'updated_at'}}
 );
 
